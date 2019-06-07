d3fad8a71b6a109abfa926b5922f721f70163ef6
Henry <cataniahf@gmail.com>
2019-06-06 22:19:28 -0400

    Student and classroom classes finished, need tests.   I know, I know, should have written them as I went.

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,95 @@
 package io.zipcoder;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+
+import static org.apache.commons.lang3.ArrayUtils.*;
+
+
 public class Classroom {
-}
+
+
+    private Student[] students;
+
+    private int maxNumberOfStudents;
+
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
+
+    public Classroom(int maxNumberOfStudents) {
+
+    }
+
+    public Classroom() {
+
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Double getAverageExamScores() {
+        Double sum = 0.0;
+        for (Student student : students) {
+            sum += student.getAvarageExamScore();
+        }
+        return sum / students.length;
+    }
+
+    public Boolean addStudent(Student student) {
+        for (Student value : students) {
+            if (value == student) {
+                if (value == null) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    public void removeStudent(String firstName, String lastName) {
+        for (Student student : students) {
+            if (firstName == student.getFirstName() && lastName == student.getLastName()) {
+                removeElement(students, student);
+
+            }
+        }
+    }
+
+        public Student[] getStudentByScore () {
+            ArrayList<Student> studentList = new ArrayList<>(Arrays.asList(students));
+            Collections.sort(studentList, Collections.reverseOrder());
+            Student[] students1 = studentList.toArray(new Student[studentList.size()]);
+
+            return students1;
+        }
+
+        public HashMap getGradeBook (){
+
+            HashMap<Student, String> gradeBook = new HashMap<Student, String>();
+            for (Student i : students) {
+                Double percentile = ((i.getAvarageExamScore() / 100) * (double) (students.length + 1));
+                if (percentile > 10) {
+                    gradeBook.put(i, "A");
+                } else if (percentile < 30) {
+                    gradeBook.put(i, "B");
+                } else if (percentile < 50) {
+                    gradeBook.put(i, "C");
+                } else if (percentile < 90) {
+                    gradeBook.put(i, "D");
+                } else {
+                    gradeBook.put(i, "F");
+                }
+            }
+
+
+            return gradeBook;
+        }
+
+    }
+
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,71 @@
 package io.zipcoder;
 
-public class Student {
+import java.util.ArrayList;
+
+public class Student implements Comparable<Student>{
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, ArrayList<Double> examScores) {
+
+    }
+
+    public void addExamScore(Double score) {
+        examScores.add(score);
+    }
+
+    public void setExamScore(int examId, Double newScore) {
+        examScores.set(examId, newScore);
+    }
+
+    public int getAvarageExamScore() {
+        int average = 0;
+        for (int i = 0; i < examScores.size(); i++) {
+            average += examScores.get(i) / examScores.size();
+        }
+        return average;
+    }
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public ArrayList<Double> getExamScores() {
+        return examScores;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setExamScores(ArrayList<Double> examScores) {
+        this.examScores = examScores;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "StudentString{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", examScores=" + examScores +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.getAvarageExamScore() > o.getAvarageExamScore() ? 1 : this.getAvarageExamScore() < o.getAvarageExamScore() ? -1 : 0;
+    }
 }
+
